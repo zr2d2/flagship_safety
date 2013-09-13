7b9f25d2b3465b5d16407dc283b78f2041f81ead
Zach Rowe <zrowe@waterfallfms.com>
2013-09-12 23:40:14 -0400

    remove attr_accessable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :cas_authenticatable, :trackable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :remember_me, :username
-
   has_and_belongs_to_many :layers
 
   ROLES = %w[admin moderator author banned]
